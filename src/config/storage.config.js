df9f4adf18fb3d9be017d5c7b49963799d12a26f
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-19 13:09:31 +0400

    storage pass

--- src/config/storage.config.js
+++ src/config/storage.config.js
@@ -1,7 +1,7 @@
 const config = {
     credentials: {
         email: process.env.STORAGE_EMAIL || 'fitnestazerbaijan@gmail.com',
-        password: process.env.STORAGE_PASSWORD || 'Fitnest@@'
+        password: process.env.STORAGE_PASSWORD || 'Fitnest123!'
     },
     server: {
         port: process.env.STORAGE_PORT || 9090,
